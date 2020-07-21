f68e49aac6ce3724e87fd5949e4f70dce0b96336
x-sinner <bikramjeet08@gmail.com>
2020-07-21 22:40:16 +0530

    Made necessary changes in Instance, nic and provider files.

--- nic.tf
+++ nic.tf
@@ -6,75 +6,98 @@ resource "azurerm_subnet" "default" {
 }
 
 resource "azurerm_network_interface" "ns-nic" {
-  name                = "netscaler-12.1-vpx489"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-  network_security_group_id = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/networkSecurityGroups/Netscaler-12.1-VPX-nsg"
-   ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
- private_ip_address_allocation = "Dynamic"
-  public_ip_address_id = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/Netscaler-12.1-VPX-ip"
+  name                      = "netscaler-12.1-vpx489"
+  location                  = azurerm_resource_group.harbakshsingh.location
+  resource_group_name       = azurerm_resource_group.harbakshsingh.name
+  network_security_group_id = azurerm_network_security_group.Netscaler-nsg.id
+  ip_configuration {
+    name      = "ipconfig1"
+    subnet_id = azurerm_subnet.default.id
+    //    subnet_id                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/Netscaler-12.1-VPX-ip"
+
   }
 }
 
 
 
 resource "azurerm_network_interface" "mi-nic" {
-  name                = "win2k19-mi236"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-   ip_configuration {
+  name                      = "win2k19-mi236"
+  location                  = azurerm_resource_group.harbakshsingh.location
+  resource_group_name       = azurerm_resource_group.harbakshsingh.name
+  network_security_group_id = azurerm_network_security_group.MI-nsg.id
+  ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.default.id
-  private_ip_address_allocation = "Dynamic" 
- }
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/Win2k19-MI-ip"
+    //     subnet_id                                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+  }
 }
 
 
 resource "azurerm_network_interface" "dc-nic" {
-  name                = "win2k19-dc01-ni"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-   ip_configuration {
+  name                          = "win2k19-dc01-ni"
+  location                      = azurerm_resource_group.harbakshsingh.location
+  resource_group_name           = azurerm_resource_group.harbakshsingh.name
+  enable_accelerated_networking = true
+  network_security_group_id     = azurerm_network_security_group.DC-nsg.id
+  ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.default.id
- private_ip_address_allocation = "Dynamic" 
-}
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/WIn2k19-DC01-ip"
+    //     subnet_id                                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+  }
+
 }
 
 
 resource "azurerm_network_interface" "dc-backup-nic" {
-  name                = "win2k19-sf-dc02810"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-   ip_configuration {
+  name                          = "win2k19-sf-dc02810"
+  location                      = azurerm_resource_group.harbakshsingh.location
+  resource_group_name           = azurerm_resource_group.harbakshsingh.name
+  enable_accelerated_networking = true
+  network_security_group_id     = azurerm_network_security_group.DC-backup-nsg.id
+  ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.default.id
- private_ip_address_allocation = "Dynamic" 
-}
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/Win2k19-SF-DC02-ip"
+    //     subnet_id = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+  }
 }
 
 resource "azurerm_network_interface" "sf-nic" {
-  name                = "win2k19-sf01768"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-   ip_configuration {
+  name                          = "win2k19-sf01768"
+  location                      = azurerm_resource_group.harbakshsingh.location
+  resource_group_name           = azurerm_resource_group.harbakshsingh.name
+  enable_accelerated_networking = true
+  network_security_group_id     = azurerm_network_security_group.SF-nsg.id
+
+  ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.default.id
- private_ip_address_allocation = "Dynamic"
-}
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/WIn2k19-DC-SF-02-ip"
+    //     subnet_id                                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+
+  }
 }
 
 
 
 resource "azurerm_network_interface" "domain-controller-nic" {
-  name                = "windows2016-ad578"
-  location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name = azurerm_resource_group.harbakshsingh.name
-   ip_configuration {
+  name                      = "windows2016-ad578"
+  location                  = azurerm_resource_group.harbakshsingh.location
+  resource_group_name       = azurerm_resource_group.harbakshsingh.name
+  network_security_group_id = azurerm_network_security_group.DomainController-nsg.id
+  ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.default.id
- private_ip_address_allocation = "Dynamic"
-}
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/publicIPAddresses/Windows2016-AD-ip"
+    //     subnet_id                                     = "/subscriptions/8d680bce-ff5f-44c3-8cd3-aa877e9585f2/resourceGroups/harbakshsingh/providers/Microsoft.Network/virtualNetworks/harbakshsingh-vnet/subnets/default"
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
 # Configure the Azure Provider
 provider "azurerm" {
-    version="1.38.0"
-    skip_provider_registration=true
+  version                    = "1.38.0"
+  skip_provider_registration = true
+
 }
 
 # create resource group
 resource "azurerm_resource_group" "harbakshsingh" {
-  name                = "harbakshsingh"
-  location            = "uksouth"
+  name     = "harbakshsingh"
+  location = "uksouth"
 }
 
 
@@ -15,11 +16,11 @@ resource "azurerm_resource_group" "harbakshsingh" {
 resource "azurerm_virtual_network" "harbakshsingh-vnet" {
   name                = "harbakshsingh-vnet"
   location            = azurerm_resource_group.harbakshsingh.location
-  resource_group_name =  "harbakshsingh"
-  address_space       = [
-              "10.0.0.0/24",
-              "192.168.1.0/24",
-              "10.1.0.0/24"
-            ]
+  resource_group_name = "harbakshsingh"
+  address_space = [
+    "10.0.0.0/24",
+    "192.168.1.0/24",
+    "10.1.0.0/24"
+  ]
 }
 
